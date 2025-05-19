24205c9f89a4d8ad15adacc1e96f0dde512aa8ee
wenrenchenchen <2106679886@qq.com>
2025-05-19 17:06:12 +0800

    头部组件的ui实现

--- src/layouts/components/FHeader.vue
+++ src/layouts/components/FHeader.vue
@@ -1,3 +1,120 @@
+<script setup>
+import { showModal } from '~/composables/util'
+import { logout } from '~/api/manager'
+import { toast } from '~/composables/util'
+import { useRouter } from 'vue-router'
+
+import { useStore } from 'vuex'
+const router = useRouter()
+const store = useStore()
+    function handleLogout(){
+        showModal("是否要退出登录?")
+        .then(res=>{
+            logout()
+            .finally(()=> {
+                //将这两个 封装到vuex的actions里 移除cookie里的token and 清除当前用户状态 vuex
+                store.dispatch("logout")
+
+                //跳转到登录页面    
+                router.push('/login')
+                //提示退出登录成功
+                toast("退出登录成功")
+
+            })
+
+        })
+
+    }
+</script>
 <template>
-    <div>头部导航</div>
+    <div class="f-header">
+
+        <span class="logo">
+            <el-icon class="logo-00"><Avatar /></el-icon>
+            AHC
+        </span>
+        <el-icon class="icon-btn"><fold /></el-icon>
+        <el-icon class="icon-btn"><refresh /></el-icon>
+        <div class="header-right">
+            <el-icon class="icon-btn"><FullScreen /></el-icon>
+            <!-- 下拉菜单 -->
+            <el-dropdown class="dropdown">
+                <span class="dropdown-link">
+                    <el-avatar class="avatar" :size="25" :src="$store.state.user.avatar" />
+                    {{ $store.state.user.username }}
+                    <el-icon class="el-icon--right">
+                        <arrow-down />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                <el-dropdown-menu>
+                    <el-dropdown-item>修改密码</el-dropdown-item>
+                    <el-dropdown-item @click="handleLogout">退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+
+        </div>
+    </div>
 </template>
+<style>
+    .f-header {
+        display: flex;
+        background-color: rgb(184, 80, 215);
+        color: hsl(214, 100%, 99%);
+        height: 64px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;   
+        align-items: center;
+    }
+    .header-right {
+        margin-left: auto;
+        display: flex;
+        align-items: center;
+    }
+    .logo {
+        display: flex;
+        width: 250px;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.1rem;
+        line-height: 1.75rem;
+        font-weight: 500;
+    }
+    .logo-00 {
+        margin-right: 0.25rem;
+    }
+    .icon-btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 42px;
+        height: 64px;
+        cursor: pointer;
+    }
+
+    .icon-btn:hover{
+        background-color: rgb(202, 136, 217);
+    }
+
+    .f-header .dropdown {
+        height: 64px;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 1rem;
+        margin-right: 1rem
+    }
+    .dropdown-link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: hsl(214, 100%, 99%);
+    }
+    .avatar {
+        margin-right: 0.5rem;
+    }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,38 +1,11 @@
 <!-- 后台首页 -->
-<script setup>
-import { showModal } from '~/composables/util'
-import { logout } from '~/api/manager'
-import { toast } from '~/composables/util'
-import { useRouter } from 'vue-router'
 
-import { useStore } from 'vuex'
-const router = useRouter()
-const store = useStore()
-    function handleLogout(){
-        showModal("是否要退出登录?")
-        .then(res=>{
-            logout()
-            .finally(()=> {
-                //将这两个 封装到vuex的actions里 移除cookie里的token and 清除当前用户状态 vuex
-                store.dispatch("logout")
-
-                //跳转到登录页面    
-                router.push('/login')
-                //提示退出登录成功
-                toast("退出登录成功")
-
-            })
-
-        })
-
-    }
-</script>
 
 <template>
     <div>
         后台首页
         {{ $store.state.user.username }}
-        <el-button @click="handleLogout">退出登录</el-button>
+
     </div>
 </template>
 
