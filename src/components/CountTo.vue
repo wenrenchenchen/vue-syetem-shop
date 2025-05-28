5bca92c318705aef91447898e4042b6c76f6da9c
wenrenchenchen <2106679886@qq.com>
2025-05-29 01:15:00 +0800

    28提交 优化布局（动画bug）
    使用骨架屏，实现数据加载动画效果
    使用第三方库gsap ，createCountTo组件实现数据滚动动画效果
    bug标记：item.value渲染错乱

---
+++ src/components/CountTo.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { reactive,watch } from 'vue';
+import gsap from 'gsap';
+
+// 滚动的最大值
+const props = defineProps({
+    value:{
+        type:Number,
+        default:0
+    }
+})
+
+
+const d = reactive({
+    num:0
+})
+
+function AnimateToValue() {
+    gsap.to(d,{
+        duration:1,
+        num:props.value
+    })
+}
+
+
+
+AnimateToValue()
+
+// 如果value变化了，就重新执行，这样就能实现动画
+watch(()=>props.value,()=>AnimateToValue())
+
+</script>
+<template>
+    {{ d.num.toFixed(0) }}
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@ import { ref } from "vue"
 import {
     getStatistics1
 } from "~/api/index.js"
-
+import CountTo from "~/components/CountTo.vue"
 
 
 const panels = ref([])
@@ -18,8 +18,31 @@ getStatistics1()
 </script>
 
 <template>
+    
     <div>
         <el-row :gutter="20">
+            <template v-if="panels.length == 0">
+                <el-col :span="6" v-for="i in 4" :key="i">
+                    <el-skeleton style="width: 100%;" animated loading>
+                        <template #template>
+                            <el-card shadow="hover" class="border-0">
+                                <template #header>
+                                    <div class="flex justify-between">
+                                        <el-skeleton-item variant="text" style="width: 50%" />
+                                        <el-skeleton-item variant="text" style="width: 10%" />
+                                    </div>
+                                </template>
+                                <el-skeleton-item variant="h3" style="width: 80%" />
+                                <el-divider />
+                                <div class="flex justify-between text-sm  text-gray-500">
+                                    <el-skeleton-item variant="text" style="width: 50%" />
+                                    <el-skeleton-item variant="text" style="width: 10%" />
+                                </div>
+                            </el-card>
+                        </template>
+                    </el-skeleton>
+                </el-col>
+            </template>
             <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
                 <el-card shadow="hover" class="border-0">
                     <template #header>
@@ -31,6 +54,7 @@ getStatistics1()
                         </div>
                     </template>
                     <span class="text-3xl font-bold text-gray-500">
+                        <!-- <CountTo :value="item.value"/> -->
                         {{ item.value }}
                     </span>
                     <el-divider />
