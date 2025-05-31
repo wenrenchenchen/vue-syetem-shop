405c2468b0d9710af755bc360ac2218a780ff38d
wenrenchenchen <2106679886@qq.com>
2025-05-31 22:35:04 +0800

    图库-初始化布局

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,29 @@
 
 <style>
     body{
-        @apply bg-gray-100
+        @apply bg-gray-100;
     }
     #nprogress .bar{
         background-color: rgb(192, 249, 250) !important;
         height: 3px !important;
+    }
+    /* 滚动条 */
+    ::-webkit-scrollbar{
+        width: 8px;
+        height: 8px;
+    }
+
+    ::-webkit-scrollbar-corner{
+        display: block;
+    }
+    ::-webkit-scrollbar-thumb{
+        border-radius: 8px;
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+    ::-webkit-scrollbar-thumb,
+    ::-webkit-scrollbar-track{
+        border-right-color: transparent;
+        border-left-color: transparent;
+        background-color: rgba(0, 0, 0, 0.1);
     }
 </style>
--- src/pages/image/list.vue
+++ src/pages/image/list.vue
@@ -1,11 +1,69 @@
 <script setup>
+const windowHeight = window.innerHeight || document.body.clientHeight
+const h = windowHeight - 64 - 44 - 40
 
 </script>
 
 <template>
-    <div>图库</div>
+
+    <el-container class="bg-white rounded" :style = "{ height: (h + 'px') }">
+        <!-- A头部 -->
+        <el-header class="image-header">Header</el-header>
+        <el-container>
+            <!-- B侧边 -->
+            <el-aside width="220px" class="image-aside">
+                <!-- 上 ：内容标题 -->
+                <div class="top">
+                    <div v-for="i in 100" :key="i">{{ i }}</div>
+                </div>
+                <!-- 下 ：分页 -->
+                <div class="bottom">
+                    分页
+                </div>
+            </el-aside>
+            <!-- C内容 -->
+            <el-main class="image-main">
+                <!-- 上 ：内容标题 -->
+                <div class="top">
+                    <div v-for="i in 100" :key="i">{{ i }}</div>
+                </div>
+                <!-- 下 ：分页 -->
+                <div class="bottom">
+                    分页
+                </div>
+            </el-main>
+        </el-container>
+    </el-container>
+
 </template>
 
 <style>
-    
+.image-header{
+    border-bottom: 1px solid #eeeeee;
+    @apply flex items-center;
+}
+
+.image-aside{
+    border-right: 1px solid #eeeeee;
+    position: relative;
+}
+.image-main {
+    position: relative;
+}
+.image-aside .top,.image-main .top{
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 10px;
+    overflow-y: auto;
+}
+.image-aside .bottom,.image-main .bottom{
+    position: absolute;
+    height: 50px;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    @apply flex items-center justify-center;
+}
 </style>
