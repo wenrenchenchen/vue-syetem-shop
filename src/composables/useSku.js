6135537a832f37a64ac1e9b8f75722aa611af6fa
wenrenchenchen <2106679886@qq.com>
2025-07-15 02:07:45 +0800

    渲染规格选项的值

--- src/composables/useSku.js
+++ src/composables/useSku.js
@@ -1,7 +1,7 @@
 import { id } from "element-plus/es/locales.mjs";
 import { ref } from "vue";
-import { 
-    createGoodsSkusCard ,
+import {
+    createGoodsSkusCard,
     updateGoodsSkusCard,
     deleteGoodsSkusCard,
     sortGoodsSkusCard
@@ -20,23 +20,23 @@ export const sku_card_list = ref([])
 
 
 //初始化规格选项列表
-export function initSkuCardList(d){
-    sku_card_list.value = d.goodsSkusCard.map(item=>{
+export function initSkuCardList(d) {
+    sku_card_list.value = d.goodsSkusCard.map(item => {
         // 用一个text做中介，来让后面name有值后 可以修改
         item.text = item.name
         item.loading = false
-        item.goodsSkusCardValue.map(v=>{
+        item.goodsSkusCardValue.map(v => {
             v.text = v.value || "属性值"
             return v
         })
         return item
     })
-    
+
 }
 
 // 添加规格选项
 export const btnLoading = ref(false)
-export function addSkuCardEvent(){
+export function addSkuCardEvent() {
     btnLoading.value = true
     createGoodsSkusCard({
         goods_id: goodsId.value,
@@ -44,67 +44,67 @@ export function addSkuCardEvent(){
         order: 50,
         type: 0
     })
-    .then(res=>{
-        sku_card_list.value.push({
-            ...res,
-            text:res.name,
-            loading:false,
-            goodsSkusCardValue:[]
+        .then(res => {
+            sku_card_list.value.push({
+                ...res,
+                text: res.name,
+                loading: false,
+                goodsSkusCardValue: []
+            })
+
         })
-        
-    })
-    .finally(()=>{
-        btnLoading.value = false
-    })
+        .finally(() => {
+            btnLoading.value = false
+        })
 }
 
 //修改规格选项
-export function handleUpdate(item){
+export function handleUpdate(item) {
     item.loading = true
-    updateGoodsSkusCard(item.id,{
+    updateGoodsSkusCard(item.id, {
         "goods_id": item.goods_id,
-        "name":item.text,
+        "name": item.text,
         "order": item.order,
         "type": 0
     })
-    .then(res=>{
-        item.name = item.text
-    })
-    .catch(err=>{
-        item.text = item.name
-    })
-    .finally(()=>{
-        item.loading = false
-    })
-    
+        .then(res => {
+            item.name = item.text
+        })
+        .catch(err => {
+            item.text = item.name
+        })
+        .finally(() => {
+            item.loading = false
+        })
+
 }
 //删除规格选项
-export function handleDelete(item){
+export function handleDelete(item) {
     item.loading = true
     deleteGoodsSkusCard(item.id)
-    .then(res=>{
-        const i = sku_card_list.value.findIndex(o => o.id == item.id )
-        if(i != -1){
-            sku_card_list.value.splice(i,1)
-        }
-    })
+        .then(res => {
+            const i = sku_card_list.value.findIndex(o => o.id == item.id)
+            if (i != -1) {
+                sku_card_list.value.splice(i, 1)
+            }
+        })
 
 }
 
 // 排序规格选项 上移和下移
 export const bodyLoading = ref(false)
-export function sortCard(action,index){
+export function sortCard(action, index) {
 
     let oList = JSON.parse(JSON.stringify(sku_card_list.value))
     let func = action == "up" ? useArrayMoveUp : useArrayMoveDown
-    func(oList,index)
+    func(oList, index)
     // if(action == "up"){
     //     useArrayMoveUp(oList,index)
     // }
     // if(action == "down"){
     //     useArrayMoveDown(oList,index)
     // }
-    let sortData = oList.map((o,i)=>{
+    let sortData = oList.map((o, i) => {
         return {
             id: o.id,
             order: i + 1
@@ -113,21 +113,53 @@ export function sortCard(action,index){
     // console.log(sortData);
     bodyLoading.value = true
     sortGoodsSkusCard({
-        sortdata:sortData
+        sortdata: sortData
     })
-    .then(res=>{
-        func(sku_card_list.value,index)
-    })
-    .finally(()=>{
-        bodyLoading.value = false
-    })
+        .then(res => {
+            func(sku_card_list.value, index)
+        })
+        .finally(() => {
+            bodyLoading.value = false
+        })
 }
 
 
 //初始化规格值
-export function initSkusCardItem(id){
-    const item = sku_card_list.value.find(o=>o.id == id)
+export function initSkusCardItem(id) {
+    const item = sku_card_list.value.find(o => o.id == id)
+
+    const inputValue = ref('')
+    const dynamicTags = ref(['Tag 1', 'Tag 2', 'Tag 3'])
+    const inputVisible = ref(false)
+    const InputRef = ref()
+
+    const handleClose = (tag) => {
+        dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1)
+    }
+
+    const showInput = () => {
+        inputVisible.value = true
+        nextTick(() => {
+            InputRef.value.input.focus()
+        })
+    }
+
+    const handleInputConfirm = () => {
+        if (inputValue.value) {
+            dynamicTags.value.push(inputValue.value)
+        }
+        inputVisible.value = false
+        inputValue.value = ''
+    }
+
     return {
-        item
+        item,
+        inputVisible,
+        inputValue,
+        InputRef,
+        handleClose,
+        showInput,
+        handleInputConfirm
+
     }
 }
--- src/pages/goods/components/SkuCardItem.vue
+++ src/pages/goods/components/SkuCardItem.vue
@@ -8,12 +8,40 @@ const props = defineProps({
     skuCardId: [Number, String]
 })
 const {
-    item
+    item,
+    inputVisible,
+    inputValue,
+    InputRef,
+    handleClose,
+    showInput,
+    handleInputConfirm
 } = initSkusCardItem(props.skuCardId)
+
+
 </script>
 
 <template>
-    <div v-for="(tag,index) in item.goodsSkusCardValue" :key="tag.id">
-        {{ tag.text }}
-    </div>
+
+    <el-tag
+      v-for="(tag,index) in item.goodsSkusCardValue"
+      :key="index"
+      closable
+      :disable-transitions="false"
+      @close="handleClose(tag)"
+      effect="plain"
+    >
+      {{ tag.text }}
+    </el-tag>
+    <el-input
+      v-if="inputVisible"
+      ref="InputRef"
+      v-model="inputValue"
+      class="w-20"
+      size="small"
+      @keyup.enter="handleInputConfirm"
+      @blur="handleInputConfirm"
+    />
+    <el-button v-else class="button-new-tag" size="small" @click="showInput">
+      + New Tag
+    </el-button>
 </template>
