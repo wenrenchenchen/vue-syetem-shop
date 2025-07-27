cdbdc196fd311f355cf0e26cdc32cc2dbe4fc7d6
wenrenchenchen <2106679886@qq.com>
2025-07-27 22:31:00 +0800

    商品分类模块-初始化

---
+++ src/pages/category/components/GoodsDrawer.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref } from 'vue';
+import { toast } from '~/composables/util'
+import FormDrawer from '~/components/FormDrawer.vue';
+import {
+    getCategoryGoods,
+    deleteCategoryGoods,
+} from '~/api/category.js';
+const formDrawerRef = ref(null);
+const category_id = ref(0);
+const tableData = ref([]);
+const open = (item) => {
+
+    category_id.value = item.id
+    item.goodsDrawerLoading = true
+    getData()
+        .then(res => {
+            formDrawerRef.value.open()
+        })
+        .finally(() => {
+            item.goodsDrawerLoading = false
+        })
+
+}
+
+function getData() {
+    return getCategoryGoods(category_id.value)
+        .then(res => {
+            // console.log(res);
+            tableData.value = res.map(o=>{
+                o.loading = false
+                return o
+            })
+        })
+
+}
+
+const handleDelete = (row) => {
+    row.loading = true
+    deleteCategoryGoods(row.id).then(res=>{
+        toast("删除成功")
+        getData()
+    })
+
+}
+
+defineExpose({
+    open,
+})
+
+</script>
+<template>
+    <FormDrawer ref="formDrawerRef" title="推荐商品">
+        <el-table :data="tableData" border stripe style="width: 100%;">
+            <el-table-column prop="goods_id" label="ID" width="60" />
+            <el-table-column label="商品封面" width="180">
+                <template #default="{ row }">
+                    <el-image :src="row.cover" fit="fill" :lazy="true" style="width: 64px;
+                    height: 64px;"></el-image>
+
+                </template>
+            </el-table-column>
+            <el-table-column prop="name" label="商品名称" width="180" />
+
+
+            <el-table-column label="操作">
+                <template #default="{ row }">
+                    <el-popconfirm title="是否要删除该记录?" confirm-button-text="确认" cancel-button-text="取消"
+                        @confirm="handleDelete(row)">
+                        <template #reference>
+                             <el-button type="primary" text size="small" :loading="row.loading" >删除</el-button>
+                        </template>
+                    </el-popconfirm>
+                   
+                </template>
+            </el-table-column>
+
+        </el-table>
+
+    </FormDrawer>
+</template>
--- src/pages/category/list.vue
+++ src/pages/category/list.vue
@@ -1,3 +1,137 @@
+<script setup>
+import { ref } from 'vue';
+
+import ListHeader from '~/components/ListHeader.vue'
+import FormDrawer from '~/components/FormDrawer.vue'
+import GoodsDrawer from './components/GoodsDrawer.vue';
+import {
+    getCategoryList,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+    updateCategoryStatus,
+    getCategoryGoods
+} from '~/api/category.js';
+
+import {
+    useInitTable,
+    useInitForm
+} from "~/composables/useCommon.js"
+
+
+const {
+    loading,
+    tableData,
+    getData,
+    handleStatusChange,
+    handleDelete
+} = useInitTable({
+    getList: getCategoryList,
+    onGetListSuccess: (res) => {
+        tableData.value = res.map(o=>{
+            o.goodsDrawerLoading = false
+            return o
+        })
+
+
+    },
+    updateStatus: updateCategoryStatus,
+    delete: deleteCategory
+})
+
+
+// 新增/修改/删除
+const {
+    drawerTitle,
+    formDrawerRef,
+    formRef,
+    form,
+    rules,
+    handleSubmit,
+    handleCreate,
+    handleEdit
+
+} = useInitForm({
+    form: {
+        name: "",
+    },
+    rules: {
+
+    },
+    getData,
+    update: updateCategory,
+    create: createCategory,
+
+})
+
+
+const GoodsDrawerRef = ref(null)
+
+const openGoodsDrawer = (data) => GoodsDrawerRef.value.open(data)
+
+</script>
 <template>
-    <div>分类列表</div>
+    <el-card shadow="never" class="border-0">
+        <!-- 新增按钮 -->
+        <ListHeader @create="handleCreate" @refresh="getData" />
+
+        <el-tree :data="tableData" :props="{ label: 'name', children: 'child' }" v-loading="loading" node-key="id"
+            >
+            <template #default="{ node, data }">
+                <div class="custom-tree-node">
+                    <!-- 左边 -->
+
+                    <span>{{ data.name }}</span>
+
+                    <!-- 右边 -->
+                    <div class="ml-auto">
+                        <el-button text type="primary" size="small" @click="openGoodsDrawer(data)" :loading="data.goodsDrawerLoading" >推荐商品</el-button>
+                        <el-switch :modelValue="data.status" :active-value="1" :inactive-value="0"
+                            @change="handleStatusChange($event, data)" />
+                        <el-button text type="primary" size="small" @click.stop="handleEdit(data)">修改</el-button>
+                        
+                        <el-popconfirm title="是否要删除该记录?" confirm-button-text="确认" cancel-button-text="取消"
+                            @confirm="handleDelete(data.id)">
+                            <template #reference>
+                                <el-button text type="primary" size="small" @click="">删除</el-button>
+                            </template>
+                        </el-popconfirm>
+
+                    </div>
+                </div>
+            </template>
+        </el-tree>
+
+
+        <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+            <el-form :model="form" ref="formRef" :rules="rules"  :inline="false">
+
+                <el-form-item label="分类名称" prop="name">
+                    <el-input v-model="form.name"  placeholder="分类名称"></el-input>
+                </el-form-item>
+
+            </el-form>
+        </FormDrawer>
+
+        <GoodsDrawer ref="GoodsDrawerRef">
+
+        </GoodsDrawer>
+    </el-card>
+
+
+
+
 </template>
+<style>
+.custom-tree-node {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    font-size: 14px;
+    padding-right: 8px;
+}
+
+.el-tree-node__content {
+    padding: 20px 0;
+}
+</style>
